30130c257efb5e3e550e02f70b876d0fcf2b4370
zoebogan <zoe.bogan@gwu.edu>
2024-10-04 17:20:24 -0400

    final commit

--- TypeC.java
+++ TypeC.java
@@ -3,22 +3,22 @@ import java.util.Random;
 
 public class TypeC extends Thing {
     protected Random rand = new Random(System.currentTimeMillis());
-    private int timeSinceLast;
+    private int threeStep;
 
     public TypeC(int row, int col) {
         super(row, col, 'm'); 
-        timeSinceLast = 0;
+        threeStep = 0;
     }
-    public void maybeTurn(Random rand) {
-        timeSinceLast++;
 
-        if (timeSinceLast == 5) {
-            timeSinceLast = 0;
-            int i = rand.nextInt(3);
-            if (i == 1) {
-                rightTurn();
-            }
+    public void maybeTurn(Random rand) {
+        step();
+        threeStep++;
+        rightTurn();
+
+        if (threeStep == 3) {
+            rightTurn();
             step();
+            threeStep = 0;
         }
     }
 }
